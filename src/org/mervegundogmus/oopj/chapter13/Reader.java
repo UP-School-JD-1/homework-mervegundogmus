e639f59a4fa653907cb900fd07fe6f0bc5caba7d
Merve Gündoğmuş <68029482+mervegundogmus@users.noreply.github.com>
2022-08-18 21:55:27 +0300

    Add files via upload

---
+++ src/org/mervegundogmus/oopj/chapter13/Reader.java
@@ -0,0 +1,41 @@
+package org.mervegundogmus.oopj.chapter13;
+public class Reader {
+	    String firstName;
+	    String lastName;
+	    int age;
+	    public Reader(String firstName,String lastName, int age) {
+	        this.firstName = firstName;
+	        this.lastName = lastName;
+	        this.age = age;
+	    }
+
+
+	    public String getFirstName() {
+	        return firstName;
+	    }
+	    
+	    public String getLastName() {
+	        return lastName;
+	    }
+
+	    public void setFirstName(String firstName) {
+	        this.firstName = firstName;
+	    }
+	    
+	    public void setLastName(String lastName) {
+	        this.lastName = lastName;
+	    }
+
+	    public int getAge() {
+	        return age;
+	    }
+
+	    public void setAge(int age) {
+	        this.age = age;
+	    }
+
+	    public void getInfo() {
+	        System.out.println("Reader: " + firstName + " , " + lastName + " , " + age );
+	    }
+}
+
